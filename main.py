8c0ce28efb0ee108ceccc5a5806ab0d76e6499b0
hydraAngel <ppvverissimo@gmail.com>
2023-09-07 01:48:11 -0300

    areas clientes e plano de manuntenção :shipit:

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def main():
     return render_template('main.html')
 
+@app.route("/clientes/abrir")
+def cliAbrir():
+    return render_template('clientes/abrir.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
